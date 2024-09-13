c7159f57e656d025e3c4f54b3dd2372527f3a564
Cassiano Sobierai <77468989+cassisob@users.noreply.github.com>
2024-09-13 00:35:50 -0300

    Main changes

---
+++ repository_client/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.client.repository_client {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.client.repository_client to javafx.fxml;
+    exports com.client.repository_client;
+}
